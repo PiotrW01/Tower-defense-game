de8cda11d02ad71ce7c9e5e144cfa03ab22eaf14
unknown <piotr140401@gmail.com>
2023-04-29 15:09:19 +0200

    m

---
+++ Assets/EnemyStuff/EnemyWave.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWave : MonoBehaviour
+{
+    public GameObject[][] enemies = new GameObject[10][]; // enemy + amount + spawnDelay
+
+    private void Start()
+    {
+       
+    }
+
+}
